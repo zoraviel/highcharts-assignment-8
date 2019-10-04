32204632e04cb74b3db68b4ba3960240387d6a93
macloo <mmcadams@gmail.com>
2019-10-04 12:57:12 -0400

    revise test_check

---
+++ scripts/test_check.js
@@ -0,0 +1,25 @@
+// code for a basic bar chart
+
+Highcharts.chart('container', {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Fruit Consumption'
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges', 'Mangoes']
+    },
+    yAxis: {
+      title: {
+        text: 'Items of fruit that were eaten last week in Susan and Scott\'s house'
+      }
+    },
+    series: [{
+      name: 'Scott',
+      data: [1, 0, 4, 2]
+    }, {
+      name: 'Susan',
+      data: [5, 7, 3, 0]
+    }]
+  });
